3905eac31f6cb4852e5a5c9d03cb4f5e60415619
Abdullah Adeel <easyautosol@gmail.com>
2021-05-27 04:13:20 +1000

    removed test cased - SinglyLinkedLists ⭕

--- LinkedLists/SinglyLinkedList/SinglyLinkedList.py
+++ LinkedLists/SinglyLinkedList/SinglyLinkedList.py
@@ -241,22 +241,5 @@ class SinglyLinkedList:
 
 
 if __name__ == "__main__":
-	llist_1 = SinglyLinkedList()
-	llist_2 = SinglyLinkedList()
-
-	llist_1.append(1)
-	llist_1.append(5)
-	llist_1.append(7)
-	llist_1.append(9)
-	llist_1.append(10)
-
-	llist_2.append(2)
-	llist_2.append(3)
-	llist_2.append(4)
-	llist_2.append(6)
-	llist_2.append(8)
-	llist_2.append(69)
-
-	llist_1.merge_sorted(llist_2)
-	llist_1.print_list()
+	pass
 
